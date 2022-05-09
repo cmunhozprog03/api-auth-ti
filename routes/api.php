e2096211c6fd0ab82036326d088aae436525c319
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-09 10:50:57 -0300

    me

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,10 @@ use Illuminate\Support\Facades\Route;
 /*
  Auth and Register Routes
 */
-Route::post('register', [RegisterController::class, 'store']);
-Route::post('auth', [AuthController::class, 'auth']);
+Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('/users', UserController::class);
